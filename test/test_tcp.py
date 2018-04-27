3b555e43b0433edaea991a358a9b8d7f8b882b7d
tor4z <vwenjie@hotmail.com>
2018-04-28 06:57:48 +0800

    Fix test tcp client

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -1,6 +1,7 @@
 import unittest
 from ion.tcpserver import TCPServer
 from ion.tcpclient import TCPClient
+from ion.eloop import ELoop
 
 class TestTCPServer(unittest.TestCase):
     def test_bind(self):
@@ -15,8 +16,22 @@ class TestTCPServer(unittest.TestCase):
         s.start()
         s.close()
 
+    def test_conn_handler(self):
+        s = TCPServer()
+        s.bind(9000)
+        s.start()
+        s.close()
+        with self.assertRaises(OSError):
+            ELoop.current().start()
+
 class TestTCPClient(unittest.TestCase):
-    pass
+    def test_send_recv(self):
+        client = TCPClient()
+        stream = client.connect("python.org", 80)
+        stream.write(b"data")
+        data = stream.read(1024).result()
+        self.assertTrue("HTTP" in str(data))
+        stream.close()
 
 class TestTCP(unittest.TestCase):
     def test_server(self):
