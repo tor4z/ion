b29b8eee12e9009d09d9d6a1f46d78b18647f8ae
tor4z <vwenjie@hotmail.com>
2018-04-27 16:38:38 +0800

    Add test clear

--- test/test_eloop.py
+++ test/test_eloop.py
@@ -3,22 +3,25 @@ from ion.eloop import ELoop
 
 class TestELoop(unittest.TestCase):
     def test_init(self):
+        ELoop.current().clear()
         e = ELoop()
         with self.assertRaises(RuntimeError):
             ELoop()
         self.assertIsNotNone(ELoop.current(False))
-        e.close()
 
     def test_current(self):
-        self.assertIsNone(ELoop.current(False))
         self.assertIsNotNone(ELoop.current())
-        ELoop.current().close()
+        with self.assertRaises(RuntimeError):
+            ELoop.current().close()
 
     def test_instance(self):
         self.assertIsNotNone(ELoop.instance())
-        ELoop.current().close()
 
     def test_close(self):
-        ELoop.instance()
-        ELoop.current().close()
+        with self.assertRaises(RuntimeError):
+            ELoop.current().close()
+
+    def test_clear(self):
+        ELoop.current()
+        ELoop.current().clear()
         self.assertIsNone(ELoop.current(False))
