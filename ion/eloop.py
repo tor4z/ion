771bf602c1504bb536dfa8fffe0c255a7e2f7923
tor4z <vwenjie@hotmail.com>
2018-04-27 16:37:41 +0800

    Update eloop

--- ion/eloop.py
+++ ion/eloop.py
@@ -1,6 +1,20 @@
 import asyncio
 import threading
 
+def status_checker(func):
+    def _exec(*args, **kwargs):
+        try:
+            obj = args[0]
+        except IndexError:
+            obj = None
+        finally:
+            if obj is None or not isinstance(obj, ELoop):
+                raise RuntimeError("status_checker ONLY can be used in ELoop.")
+        if not obj._started:
+            raise RuntimeError("ELoop not started.")
+        func(*args, **kwargs)
+    return _exec
+
 class ELoop:
     _INSTANCE_NAME = "instance"
     _CURRENT = threading.local()
@@ -8,6 +22,7 @@ class ELoop:
     def __init__(self, loop=None):
         if hasattr(self._CURRENT, self._INSTANCE_NAME):
             raise RuntimeError("ELoop instance exist.")
+        self._started = False
         self._async_loop = loop or asyncio.new_event_loop()
         self._CURRENT.instance = self
 
@@ -33,25 +48,40 @@ class ELoop:
         return instance
 
     def start(self):
+        if self._satrted:
+            raise RuntimeError("ELoop can not be start twice.")
         try:
             old_loop = asyncio.get_event_loop()
         except RuntimeError:
             old_loop = None
         try:
+            self._started = True
             asyncio.set_event_loop(self._async_loop)
             self._async_loop.run_forever()
         finally:
             asyncio.set_event_loop(old_loop)
 
+    def clear(self):
+        if self._started:
+            self.close()
+        try:
+            del self._CURRENT.instance
+        except AttributeError:
+            pass
+
+    @status_checker
     def close(self):
         self._async_loop.close()
-        del self._CURRENT.instance
+        self._started = False
 
-    def add_reader(self):
-        pass
+    def add_reader(self, fd, callback, *args):
+        self._async_loop.add_reader(fd, callback, *args)
 
-    def add_writer(self):
-        pass
+    def remove_reader(self, fd):
+        self._async_loop.remove_reader(fd)
 
-    def add_handler(self):
-        pass
+    def add_writer(self, fd, callback, *args):
+        self._async_loop.add_writer(fd, callback, *args)
+
+    def remove_writer(self, fd):
+        self._async_loop.remove_writer(fd)
