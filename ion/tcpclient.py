12b6a6b723217a58497a9be6955483b5a49f85a6
tor4z <vwenjie@hotmail.com>
2018-04-28 06:57:08 +0800

    tcp client use iostream

--- ion/tcpclient.py
+++ ion/tcpclient.py
@@ -1,53 +1,42 @@
 import socket
+from .eloop import ELoop
+from .iostream import TCPStream
 
 class TCPClient:
-    def __init__(self, family, type):
+    """
+    Basic Example:
+    tcp = TCPClient()
+    stream = tcp.connect("python.org", 80)
+    stream.write("some data")
+    print(stream.read(1024))
+    stream.close()
+    """
+    def __init__(self, family=None, type=None):
         self._family = family or socket.AF_INET
         self._type = type or socket.SOCK_STREAM
         self._proto = socket.IPPROTO_TCP
-        self._port = None
-        self._addr = None
-        self._sock = None
-        self._closed = False
 
     def connect(self, addr, port):
-        self._addr = addr
-        self._port = port
-        self._sock = conn_socket(self._family, self._addr, self._type, 
-                                self._proto, self._port, 0)
+        sock, sockaddr = self.get_socket(addr, port)
+        return self._new_stream(sock, sockaddr)
 
-    def send(self, *args, **kwargs):
-        if not isinstance(data, bytes):
-            raise TypeError("Byte required.")
-        self._sock.send(*args, **kwargs)
-        
-    def recv(self, *args, **kwargs):
-        return self._sock.recv(*args, **kwargs)
+    def _new_stream(self, sock, sockaddr):
+        stream = TCPStream(sock, sockaddr)
+        stream.connect()
+        return stream
 
-    def close(self):
-        if self._closed:
-            raise RuntimeError("TCP client already closed.")
-        self._sock.close()
-        self._closed = True
-
-def conn_socket(family, addr, type, proto, port, flags):
-    sock = None
-    for item in set(socket.getaddrinfo(addr, port, family, type, proto, flags)):
-        family, type, proto, canonname, sockaddr = item
-        try:
-            sock = socket.socket(family, type, proto)
-        except OSError as e:
-            sock = None
-            continue
-        try:
-            sock.connect(sockaddr)
-        except OSError as e:
-            sock.close()
-            sock = None
-            continue
-        break
-    if sock is None:
-        raise TCPClientError("Could not connect to {0}:{1}.".format(addr, port))
+    def get_socket(self, addr, port):
+        sockaddr = None
+        for item in set(socket.getaddrinfo(addr, port, self._family, self._type, self._proto, 0)):
+            family, type, proto, canonname, sockaddr = item
+            try:
+                sock = socket.socket(family, type, proto)
+            except OSError as e:
+                sock = None
+                continue
+        if sock is None:
+            raise TCPClientError("Could not connect to {0}:{1}.".format(addr, port))
+        return sock, sockaddr
 
 class TCPClientError(Exception):
     pass
