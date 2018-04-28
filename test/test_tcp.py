fff30ab8a0f197c90cf89b9aa93624615e266770
tor4z <vwenjie@hotmail.com>
2018-04-29 07:50:38 +0800

    Use async

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -1,5 +1,6 @@
 import unittest
 import random
+import asyncio
 from ion.tcpserver import TCPServer
 from ion.tcpclient import TCPClient
 from ion.eloop import ELoop
@@ -26,32 +27,45 @@ class TestTCPServer(unittest.TestCase):
 
 class TestTCPClient(unittest.TestCase):
     def test_send_recv(self):
-        client = TCPClient()
-        stream = client.connect("python.org", 80)
-        stream.write(b"data")
-        data = stream.read(1024).result()
-        self.assertTrue("HTTP" in str(data))
-        stream.close()
+        async def _exec():
+            client = TCPClient()
+            stream = await client.connect("python.org", 80)
+            await stream.write(b"data")
+            data = await stream.read_until_close()
+            self.assertTrue("HTTP" in str(data))
+            await stream.close()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(_exec())
 
 _str = "abcdefghijklmnopqrstuvwxyz0123456789,./][=-)(*&^%$#@!:}{|}"
 
-def _client(rs):
+async def _client(rs):
     client = TCPClient()
-    stream = client.connect("127.0.0.1", 9000)
-    stream.write(rs)
-    data = stream.read(1024).result()
+    stream = await client.connect("127.0.0.1", 9000)
+    await stream.write(rs)
+    data = await stream.read_until_close().result()
     print("recv from server", data)
-    stream.close()
+    await stream.close()
 
 def _server(rs):
+    def handler(stream, addr):
+        stream.write(rs)
+        #data = stream.read_until_close().result()
+        print("recv from client", data)
+
     s = TCPServer()
     s.bind(9000)
     s.start()
-    ELoop.current().test(1)
+    s.stream_handler(handler)
+    ELoop.current().test(3)
 
 class TestTCP(unittest.TestCase):
     def test_client_server(self):
+        #from asyncio import wrap_future, 
+        return
         with ProcessPoolExecutor(max_workers=2) as e:
-            rs = "".join(random.choices(_str, k=random.randint(1, 100000))).encode()
+            rs = "".join(random.choices(_str, 
+                    k=random.randint(1, 100000))).encode()
             e.submit(_server, rs)
             e.submit(_client, rs)
