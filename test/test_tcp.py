ae4d8604998b962ef60b88d813b9b037971d3dac
tor4z <vwenjie@hotmail.com>
2018-04-28 15:16:48 +0800

    Add test client server

--- test/test_tcp.py
+++ test/test_tcp.py
@@ -1,7 +1,9 @@
 import unittest
+import random
 from ion.tcpserver import TCPServer
 from ion.tcpclient import TCPClient
 from ion.eloop import ELoop
+from concurrent.futures import ProcessPoolExecutor
 
 class TestTCPServer(unittest.TestCase):
     def test_bind(self):
@@ -31,6 +33,25 @@ class TestTCPClient(unittest.TestCase):
         self.assertTrue("HTTP" in str(data))
         stream.close()
 
+_str = "abcdefghijklmnopqrstuvwxyz0123456789,./][=-)(*&^%$#@!:}{|}"
+
+def _client(rs):
+    client = TCPClient()
+    stream = client.connect("127.0.0.1", 9000)
+    stream.write(rs)
+    data = stream.read(1024).result()
+    print("recv from server", data)
+    stream.close()
+
+def _server(rs):
+    s = TCPServer()
+    s.bind(9000)
+    s.start()
+    ELoop.current().test(1)
+
 class TestTCP(unittest.TestCase):
-    def test_server(self):
-        pass
+    def test_client_server(self):
+        with ProcessPoolExecutor(max_workers=2) as e:
+            rs = "".join(random.choices(_str, k=random.randint(1, 100000))).encode()
+            e.submit(_server, rs)
+            e.submit(_client, rs)
