a5682d000df6afa518c799f0936422ed466d2754
tor4z <vwenjie@hotmail.com>
2018-04-29 07:50:52 +0800

    Add test current

--- test/test_eloop.py
+++ test/test_eloop.py
@@ -1,4 +1,5 @@
 import unittest
+import asyncio
 from ion.eloop import ELoop
 
 class TestELoop(unittest.TestCase):
@@ -11,17 +12,15 @@ class TestELoop(unittest.TestCase):
 
     def test_current(self):
         self.assertIsNotNone(ELoop.current())
-        with self.assertRaises(RuntimeError):
-            ELoop.current().close()
 
     def test_instance(self):
         self.assertIsNotNone(ELoop.instance())
 
-    def test_close(self):
-        with self.assertRaises(RuntimeError):
-            ELoop.current().close()
-
     def test_clear(self):
-        ELoop.current()
         ELoop.current().clear()
         self.assertIsNone(ELoop.current(False))
+
+    def test_current(self):
+        loop = asyncio.get_event_loop()
+        eloop = ELoop.current()
+        self.assertEqual(loop, eloop._async_loop)
